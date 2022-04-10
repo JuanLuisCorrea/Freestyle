e858651f86f07cb7859c1d9af990c06274b0ceca
Juan Correa <jucs0709@gmail.com>
2022-04-10 09:28:29 -0500

    Agendar cita completado

--- ControladorAgendar.php
+++ ControladorAgendar.php
@@ -8,32 +8,59 @@
   $date = $_POST["date"];
   $hour = $_POST["hour"];
   $duracion = 0;
+  $duracion_total = 0;
  
- 
-
-
   //Base de datos
   $servername = "localhost";
   $username = "root";
   $password = "";
   $dbname = "freestyle";
   $conn = new mysqli($servername, $username, $password, $dbname);
-  if(isset($_GET["corte_de_pelo"])){
-    $sql = "SELECT Duration_Service FROM servicios WHERE Type_Service ='Corte de cabello'";
+  if(isset($_REQUEST["corte_de_pelo"])){
+    $sql = "SELECT Duration_Service FROM servicio WHERE Type_Service ='Corte de cabello'";
     
     $result = $conn->query($sql);
     
-    $duracion =  $result->fetch_assoc();
+    $duracion = $result->fetch_assoc();
+
+    $duracion_total = $duracion_total + intval($duracion["Duration_Service"]);
     
-   }
+  }
+  if(isset($_REQUEST["corte_de_barba"])){
+    $sql = "SELECT Duration_Service FROM servicio WHERE Type_Service ='Corte barba'";
+    
+    $result = $conn->query($sql);
+    
+    $duracion = $result->fetch_assoc();
+    
+    $duracion_total = $duracion_total + intval($duracion["Duration_Service"]);
+  }
+  if(isset($_REQUEST["mascarilla_facial"])){
+    $sql = "SELECT Duration_Service FROM servicio WHERE Type_Service ='Mascarilla facial'";
+    
+    $result = $conn->query($sql);
+    
+    $duracion = $result->fetch_assoc();
+    
+    $duracion_total = $duracion_total + intval($duracion["Duration_Service"]);
+  }
+  if(isset($_REQUEST["cejas"])){
+    $sql = "SELECT Duration_Service FROM servicio WHERE Type_Service ='Cejas'";
+    
+    $result = $conn->query($sql);
+    
+    $duracion = $result->fetch_assoc();
+    
+    $duracion_total = $duracion_total + intval($duracion["Duration_Service"]);
+  }
   //Crear conexión
   
 
-  $sql = "INSERT INTO cita(Date,Hour,Duration) values('".$date."','".$hour."','".$duracion."')";
+  $sql = "INSERT INTO cita(Date,Hour,Duration) values('".$date."','".$hour."','".$duracion_total."')";
     if($conn->query($sql) === TRUE) {
       include("menu.html");
       
-      echo "Registro exitoso!";
+      echo "Cita agendada!";
 
 
     
