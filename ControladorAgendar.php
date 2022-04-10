66b84f67524b194f0f67ad3454540accda0c6ac2
Juan Correa <jucs0709@gmail.com>
2022-04-10 13:00:53 -0500

    Agendar citas corregido

--- ControladorAgendar.php
+++ ControladorAgendar.php
@@ -21,53 +21,40 @@
   //Calcular duración de la cita según los servicios
   if(isset($_REQUEST["corte_de_pelo"])){
     $sql = "SELECT Duration_Service FROM servicio WHERE Type_Service ='Corte de cabello'";
-    
     $result = $conn->query($sql);
-    
     $duracion = $result->fetch_assoc();
-
     $duracion_total = $duracion_total + intval($duracion["Duration_Service"]);
-    
   }
   if(isset($_REQUEST["corte_de_barba"])){
     $sql = "SELECT Duration_Service FROM servicio WHERE Type_Service ='Corte barba'";
-    
     $result = $conn->query($sql);
-    
     $duracion = $result->fetch_assoc();
-    
     $duracion_total = $duracion_total + intval($duracion["Duration_Service"]);
   }
   if(isset($_REQUEST["mascarilla_facial"])){
     $sql = "SELECT Duration_Service FROM servicio WHERE Type_Service ='Mascarilla facial'";
-    
     $result = $conn->query($sql);
-    
     $duracion = $result->fetch_assoc();
-    
     $duracion_total = $duracion_total + intval($duracion["Duration_Service"]);
   }
   if(isset($_REQUEST["cejas"])){
     $sql = "SELECT Duration_Service FROM servicio WHERE Type_Service ='Cejas'";
-    
     $result = $conn->query($sql);
-    
     $duracion = $result->fetch_assoc();
-    
     $duracion_total = $duracion_total + intval($duracion["Duration_Service"]);
   }
 
   //Insertar cita en la base de datos
   $sql = "INSERT INTO cita(Client,Date,Hour,Duration) values('".$cc."','".$date."','".$hour."','".$duracion_total."')";
-    if($conn->query($sql) === TRUE) {
-      include("menu.html");
-      echo "Cita agendada!";
-    }
-    else {
-      echo "Error ".$conn->error;
-    }
+  if($conn->query($sql) === TRUE) {
+    include("menu.html");
+    echo "Cita agendada!";
+  }
+  else {
+    echo "Error ".$conn->error;
+  }
 
-    $conn->close();
+  $conn->close();
   ?>
 
   
