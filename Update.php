83daabbb630e9dea712c85d2bf50fdfa455e775c
LuisAngelBC <103284677+LuisAngelBC@users.noreply.github.com>
2022-04-10 23:40:53 -0500

    creacion de update

--- Update.php
+++ Update.php
@@ -10,22 +10,21 @@ $dbname = "freestyle";
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
 // Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
-
-
 if(isset($_GET['id'])){
     $id = $_GET['id'];
-    $sql = "DELETE from cita WHERE ID = '$id'";
-    $result = $conn->query($sql);
-    if(!$result){
-        die("Delete fallido!");
-    }
-
-    header("Location: Listar_Citas.php");
-
-    }
-
+}
 
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
